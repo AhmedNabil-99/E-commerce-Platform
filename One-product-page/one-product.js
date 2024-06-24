f9523ec086aed55e31d32f7dd273ce9a1fc1125a
AhmedNabil-99 <95183285+AhmedNabil-99@users.noreply.github.com>
2024-06-24 11:13:56 +0300

    Update one-product.js

--- One-product-page/one-product.js
+++ One-product-page/one-product.js
@@ -4,27 +4,35 @@ fetch('http://dummyjson.com/products/15')
     console.log(data);
     const productDetails1 = document.getElementById('product-details1');
     const productDetails2 = document.getElementById('product-details2');
-    const productDetails3 = document.getElementById('product-details3');
-    const productDetails4 = document.getElementById('product-details4');
+    // const productDetails1 = document.getElementById('product-details3');
+    // const productDetails1 = document.getElementById('product-details4');
+    // const productDetails1 = document.getElementById('product-details5');
+
     const productImage = document.createElement('img');
     const productName = document.createElement('p');
     const productDescription = document.createElement('p');
-    const productRating = document.createElement('div');
+    const productRating = document.createElement('p');
+    const productPrice = document.createElement('p');
 
+    // the img
     productImage.src = data.thumbnail;
-    productImage.alt = data.title;
+    // productImage.alt = data.title;
+    // the name
     productName.textContent = data.brand;
+    // the Description
     productDescription.textContent = `Description: ${data.description}`;
+    // the rating
     productRating.textContent = `Rating: ${data.rating}`;
+    productPrice.textContent = `Price: $${data.price}`;
 
     productDetails1.appendChild(productImage);
     productDetails2.appendChild(productName);
-    productDetails3.appendChild(productDescription);
-    productDetails4.appendChild(productRating);
+    productDetails2.appendChild(productDescription);
+    productDetails2.appendChild(productRating);
+    productDetails2.appendChild(productPrice);
     
 
     document.getElementById('loading').style.display = 'none';
     document.querySelector('.product-container').style.display = 'block';
 })
 .catch(error => console.error('Error:', error));
-
